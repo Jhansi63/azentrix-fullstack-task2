9a56ee68471aab0768b2786f41b364fbedf4762e
Jhansi63 <2200030933@kluniversity.in>
2026-06-23 19:48:36 +0530

    Fix news

--- src/services/newsApi.js
+++ src/services/newsApi.js
@@ -3,10 +3,13 @@ import axios from "axios";
 const API_KEY = import.meta.env.VITE_GNEWS_API_KEY;
 
 export const fetchNews = async (category) => {
+  console.log("API KEY:", API_KEY);
 
   const response = await axios.get(
     `https://gnews.io/api/v4/top-headlines?category=${category}&lang=en&apikey=${API_KEY}`
   );
+
+  console.log(response.data);
 
   return response.data.articles;
 };
